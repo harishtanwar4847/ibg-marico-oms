de11f1544266c2e3a0b68ad30fa87176a31a782d
Harish Tanwar <harish.tanwar@atriina.com>
2023-04-25 06:58:06 +0000

    Update __init__.py

--- ibg_marico_oms/__init__.py
+++ ibg_marico_oms/__init__.py
@@ -4,7 +4,7 @@ import frappe
 import re
 import pyodbc as p
 
-__version__ = '1.1.1'
+__version__ = '1.1.1-uat'
 
 def download_file(dataframe, file_name, file_extention, sheet_name):
     file_name = "{}.{}".format(file_name, file_extention)
