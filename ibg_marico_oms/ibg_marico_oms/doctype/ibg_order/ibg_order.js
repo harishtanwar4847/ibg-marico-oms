e8574dfb2d6aba2b261efa4c0918d43b0a23d854
Aditi Patade <aditi.patade@atriina.com>
2023-07-13 13:15:53 +0530

    Fix: Date Validation

--- ibg_marico_oms/ibg_marico_oms/doctype/ibg_order/ibg_order.js
+++ ibg_marico_oms/ibg_marico_oms/doctype/ibg_order/ibg_order.js
@@ -139,6 +139,15 @@ frappe.ui.form.on('IBG Order', {
             d.show();          
         }
     },
+	on_load: function (frm) {
+		frappe.call({
+		  method: "ibg_marico_oms.ibg_marico_oms.doctype.ibg_order.ibg_order.fetch_price_data",
+		  freeze: true,
+		  args: {
+			doc: frm.doc.name,
+		  }
+		});  
+	},	
 	order_items_on_form_rendered(frm, cdt, cdn) {
 		var is_true = frappe.user_roles.find((role) => role === "Supply Chain");
 		var is_fin = frappe.user_roles.find((role) => role === "IBG Finance");
