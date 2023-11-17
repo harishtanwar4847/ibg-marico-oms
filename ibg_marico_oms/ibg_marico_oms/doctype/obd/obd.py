4f765a47749fc9bb6518a46988d1b99670a98d61
Aditi Patade <aditi.patade@atriina.com>
2023-11-17 19:02:02 +0530

    Feat : OBD CR

--- ibg_marico_oms/ibg_marico_oms/doctype/obd/obd.py
+++ ibg_marico_oms/ibg_marico_oms/doctype/obd/obd.py
@@ -146,8 +146,7 @@ def order_reject(doc):
                 for i in order_details[1:]:
                     if str(i["SALES_ORDER"]) == str(doc.sap_so_number) and int(i["SALES_ITEM"]) == int(item.sales_item) and int(item.fg_code) == int(i["FG_CODE"]):
                         # item_doc = frappe.get_doc("OBD Items", item.name)
-                        rejected_qty = float(i["REJECTED_QTY"])
-                        item.rejected_qty = rejected_qty
+                        item.rejected_qty = i["REJECTED_QTY"]
                         item.reason_of_reject = i["REASON_OF_REJECT"]
                         item.order_status = "Fully serviced" if item.reason_of_reject else "Partial serviced"
                         item.final_status = "Completed" if item.reason_of_reject else "Pending"
